4da8723691d025ebbf4f83661aee5c918a64f5ac
Trevor Byington <tabyington@gmail.com>
2025-05-15 02:09:41 -0600

    Getting setting up structure figuring things out.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+mod systems;
+mod plugins;
+
+use bevy::prelude::*;
+use plugins::physics;
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
+        app.add_systems(Update, hello_world);
+    }
 }
+
+fn main() {
+    //println!("Hello, world!");
+    App::new()
+    .add_plugins(HelloPlugin)
+    .add_plugins(DefaultPlugins)
+    .run();
+
+}
+
+fn hello_world(time: Res<Time>, mut timer: ResMut<GreetTimer>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        println!("hello world!");
+    }   
+}
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
---
+++ src/systems/gravity.rs
@@ -0,0 +1,3 @@
+pub fn terrestial_gravity(){
+    println!("You fall at 8 meters per second per second");
+}
